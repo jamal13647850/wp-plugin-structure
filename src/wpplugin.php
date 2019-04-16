09e62887e95dab64b6277f1fa83b1a5a25b6efd9
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2019-04-16 16:12:38 +0430

    first classes

---
+++ src/wpplugin.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: j.ghasemi
+ * Date: 2/5/2019
+ * Time: 4:31 PM
+ */
+
+namespace safir\certificate;
+
+
+use gosafir\twigExtension;
+use Twig_Environment;
+use Twig_Extension_Debug;
+use Twig_Loader_Filesystem;
+
+abstract class wpplugin
+{
+    private $domain,$LangsFilesPath;
+    public function __construct($domain,$LangsFilesPath='',$file){
+
+        defined( 'ABSPATH' ) || exit;
+        $this->domain = $domain;
+        $this->LangsFilesPath = $domain;
+
+        if($LangsFilesPath==''){
+            $LangsFilesPath = dirname( plugin_basename( __FILE__ ) ) . '/langs/';
+        }
+        $this->loadPluginTextdomain($domain,$LangsFilesPath);
+
+
+        add_action('wp_enqueue_scripts', array($this,'registerScriptsAndStyles'),1);
+        add_action( 'admin_enqueue_scripts', array($this,'registerAdminScriptsAndStyles'), 10000 );
+        add_action( 'login_enqueue_scripts', [$this,'loginStylesheet'] );
+
+        register_activation_hook($file ,$this->activation());
+        register_deactivation_hook($file ,$this->deactivation());
+    }
+    public function loadPluginTextdomain($domain,$path ){
+        load_plugin_textdomain( $domain, false, $path );
+    }
+
+    public function getPluginPath($dirName){
+        $dir = trailingslashit(  plugin_dir_path( __FILE__ )."../"  . $dirName ) ;
+        $url = trailingslashit(  plugin_dir_url ( __FILE__ ) . "../". $dirName ) ;
+        return [
+            'dir'=>$dir,
+            'url'=>$url
+        ];
+    }
+    abstract function loginStylesheet();
+    abstract function registerScriptsAndStyles();
+    abstract function registerAdminScriptsAndStyles();
+
+    abstract function activation();
+    abstract function deactivation();
+
+    public function initTwig($dirs,$dirPath){
+        $loader = new Twig_Loader_Filesystem();
+
+        foreach ($dirs as $dir){
+            $loader->addPath($dirPath.$dir, $dir);
+        }
+
+        $twig = new Twig_Environment($loader, array('debug' => true));
+        $twig->addExtension(new Twig_Extension_Debug());
+        //$twig->addExtension(new twigExtension());
+
+        return $twig;
+    }
+}
---
+++ src/wpshortcodes.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: j.ghasemi
+ * Date: 2/6/2019
+ * Time: 11:22 AM
+ */
+
+namespace safir\certificate;
+
+
+abstract class wpshortcodes
+{
+    protected $tag;
+    public function __construct($tag){
+        $this->tag=$tag;
+        $this->Register();
+    }
+
+    public function Register(){
+        add_action( 'init', array($this,'AllShortCodes'));
+    }
+    //add_action( 'init', array($this,'AllShortCodes'));
+     function AllShortCodes(){
+         add_shortcode($this->tag,array($this,'mainFunction'));
+     }
+    //add_shortcode('safircode',array($this,'safirCodeFunc'));
+    abstract function mainFunction($atts);
+    /*function mainFunction($atts) {
+        $a = shortcode_atts(array(
+            'form' => ''
+        ), $atts);
+        $form = $a['form'];
+
+        switch ($form) {
+            case 'placement':
+                $placement=new placement();
+                $placement->startPlacement();
+                break;
+                break;
+        }
+    }*/
+
+}
