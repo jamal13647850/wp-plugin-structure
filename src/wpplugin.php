40ab2f60d5e5abd4de6cc97776f26f21cef7abcf
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-08-10 14:48:31 +0430

    Some changes for php 8

--- src/wpplugin.php
+++ src/wpplugin.php
@@ -17,35 +17,38 @@ use Twig_Loader_Filesystem;
 
 abstract class wpplugin
 {
-    private static $domain,$LangsFilesPath,$mode;
-    
-    public function __construct($domain,$file,$mode,$LangsFilesPath=''){
+    private static $domain, $LangsFilesPath, $mode;
 
-        defined( 'ABSPATH' ) || exit;
+    public function __construct($domain, $file, $mode, $LangsFilesPath = '')
+    {
+
+        defined('ABSPATH') || exit;
         self::$domain = $domain;
         self::$mode = $mode;
         self::$LangsFilesPath = $LangsFilesPath;
-        self::$LangsFilesPath = $LangsFilesPath==''?dirname( plugin_basename( $file ) ) . '/langs/':$LangsFilesPath;
-        
-        $this->loadPluginTextdomain($domain,self::$LangsFilesPath);
-        
-        add_action('wp_enqueue_scripts', array($this,'registerScriptsAndStyles'),1);
-        add_action( 'admin_enqueue_scripts', array($this,'registerAdminScriptsAndStyles'), 10000 );
-        add_action( 'login_enqueue_scripts', [$this,'loginStylesheet'] );
+        self::$LangsFilesPath = $LangsFilesPath == '' ? dirname(plugin_basename($file)) . '/langs/' : $LangsFilesPath;
 
-        register_activation_hook($file ,$this->activation());
-        register_deactivation_hook($file ,$this->deactivation());
+        $this->loadPluginTextdomain($domain, self::$LangsFilesPath);
+
+        add_action('wp_enqueue_scripts', array($this, 'registerScriptsAndStyles'), 1);
+        add_action('admin_enqueue_scripts', array($this, 'registerAdminScriptsAndStyles'), 10000);
+        add_action('login_enqueue_scripts', [$this, 'loginStylesheet']);
+
+        register_activation_hook($file, [$this,'activation']);
+        register_deactivation_hook($file, [$this,'deactivation']);
     }
-    public function loadPluginTextdomain($domain,$path ){
-        load_plugin_textdomain( $domain, false, $path );
+    public function loadPluginTextdomain($domain, $path)
+    {
+        load_plugin_textdomain($domain, false, $path);
     }
 
-    public function getPluginPath($dirName){
-        $dir = trailingslashit(  plugin_dir_path( __FILE__ )."../"  . $dirName ) ;
-        $url = trailingslashit(  plugin_dir_url ( __FILE__ ) . "../". $dirName ) ;
+    public function getPluginPath($dirName)
+    {
+        $dir = trailingslashit(plugin_dir_path(__FILE__) . "../"  . $dirName);
+        $url = trailingslashit(plugin_dir_url(__FILE__) . "../" . $dirName);
         return [
-            'dir'=>$dir,
-            'url'=>$url
+            'dir' => $dir,
+            'url' => $url
         ];
     }
     abstract function loginStylesheet();
@@ -55,25 +58,26 @@ abstract class wpplugin
     abstract function activation();
     abstract function deactivation();
 
-    public function initTwig($dirs,$dirPath){
+    public function initTwig($dirs, $dirPath)
+    {
         $loader = new \Twig\Loader\FilesystemLoader();
 
-        foreach ($dirs as $dir){
-            $loader->addPath($dirPath.$dir, $dir);
+        foreach ($dirs as $dir) {
+            $loader->addPath($dirPath . $dir, $dir);
         }
 
         $twig = new \Twig\Environment($loader, [
             'debug' => true,
         ]);
         $twig->addExtension(new \Twig\Extension\DebugExtension());
-        
+
         //$twig->addExtension(new twigExtension());
         return $twig;
     }
 
     /**
      * Get the value of domain
-     */ 
+     */
     public static function getDomain()
     {
         return self::$domain;
@@ -81,7 +85,7 @@ abstract class wpplugin
 
     /**
      * Get the value of mode
-     */ 
+     */
     public static function getMode()
     {
         return self::$mode;
