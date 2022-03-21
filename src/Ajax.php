509eeb38d662a4e243737cec1784bced256a83a4
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-03-21 12:26:59 +0330

    fix ajax

--- src/Ajax.php
+++ src/Ajax.php
@@ -4,8 +4,12 @@ namespace jamal\wpmstructure;
 abstract class Ajax {
     public function __construct()
     {
-        add_action('wp_ajax_'.static::class,[$this,'loggedInUsers']);
-        add_action( 'wp_ajax_nopriv_'.static::class, [$this,'loggedOutUsers'] );
+        $extendedClassName=explode('\\', static::class);
+        $extendedClassName=end($extendedClassName);
+        add_action('wp_ajax_'.$extendedClassName,[$this,'loggedInUsers']);
+        add_action( 'wp_ajax_nopriv_'.$extendedClassName, [$this,'loggedOutUsers'] );
+        
+       
     }
     abstract function loggedInUsers();
     abstract function loggedOutUsers();
